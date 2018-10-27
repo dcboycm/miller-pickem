4164f21222e99a1fe3ff6c33721dc5b64a1e6ac0
Curtis Miller <curtischristophermiller@gmail.com>
2018-10-26 21:17:10 -0700

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img src="./assets/brand_logo.png">
+    <router-link :to="{ name: 'Hello' }">Home</router-link>
+    <router-link to="/about">About</router-link>
+    <router-link to="/matchups/ethereum">Ethereum</router-link>
+    <router-link to="/matchups/bitcoin">Bitcoin</router-link>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name: 'app'
 }
 </script>
 
---
+++ src/components/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <h1>What is a Miller's Pickem?</h1>
+    <p>
+      Miller's Pickem is a digital Office Football Pool.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About'
+}
+</script>
